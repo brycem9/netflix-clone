bf3149d3d470628f14b8ff8d9a372790abe075f6
brycem9 <123997306+brycem9@users.noreply.github.com>
2023-07-15 22:40:40 -0400

    first commit

---
+++ components/Thumbnail.tsx
@@ -0,0 +1,29 @@
+import { Movie } from "@/typings"
+import Image from "next/image"
+
+
+interface Props {
+    // movie: Movie | DocumentData
+    movie: Movie 
+}
+
+function Thumbnail({ movie }: Props) {
+    console.log(movie);
+
+
+
+    return (
+    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105 ">
+        <Image
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.backdrop_path || movie.poster_path
+        }`} alt="" width={10000} height={10000}
+        className="rounded-sm object-cover md:rounded"
+        
+      />
+    </div>
+    
+  )
+}
+
+export default Thumbnail
